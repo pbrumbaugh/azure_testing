e9ac60de04f2672ecc77738b7301e58d5488b068
Brumbaugh <pbrumbau@uscvgnb30067.insight.com>
2019-01-18 11:02:26 -0500

    adding junk

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@ provider "azurerm" {
 
   subscription_id = "00000000-0000-0000-0000-000000000000"
 }
+# Create a resource group
+resource "azurerm_resource_group" "test" {
+  name     = "PSBTest2"
+  location = "East US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "test" {
+  name                = "PSBTest2-network"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  address_space       = ["10.0.0.0/16"]
+}
